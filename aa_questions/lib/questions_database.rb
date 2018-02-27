79995d71826fb18ce559dac6e0bcc6cc8c728d10
Samuel Chia <cheeeya@berkeley.edu>
2018-02-27 13:34:35 -0800

    Created Question initialization

--- aa_questions/lib/questions_database.rb
+++ aa_questions/lib/questions_database.rb
@@ -38,9 +38,20 @@ class Question
   attr_accessor :title, :body, :user_id
 
   def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        id = ?
+    SQL
+
+    data.map { |datum| Question.new(datum) }
   end
 
   def initialize(options)
+    @id = options['id']
     @title = options['title']
     @body = options['body']
     @user_id = options['user_id']
