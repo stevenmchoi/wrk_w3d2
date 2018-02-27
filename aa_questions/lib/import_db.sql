ef76a84e3b36b5eb8405c69c92796c0f6954150c
Samuel Chia <cheeeya@berkeley.edu>
2018-02-27 11:49:10 -0800

    Created database

--- aa_questions/lib/import_db.sql
+++ aa_questions/lib/import_db.sql
@@ -8,7 +8,7 @@ CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
-  user_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL
 );
 
 CREATE TABLE question_follows (
@@ -30,7 +30,7 @@ CREATE TABLE replies (
   FOREIGN KEY (question_id) REFERENCES questions(id),
   FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
 );
-user_id INTEGER, NOT NULL,
+
 
 
 CREATE TABLE question_likes (
