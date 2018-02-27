aba4f94eb26f51c58f07861f8f984f084c5fb6d0
Samuel Chia <cheeeya@berkeley.edu>
2018-02-27 14:54:06 -0800

    Create #authored_questions and #authored_replies and ::find_by_name for User class

--- aa_questions/lib/questions_database.rb
+++ aa_questions/lib/questions_database.rb
@@ -32,6 +32,27 @@ class User
     @fname = options['fname']
     @lname = options['lname']
   end
+
+  def self.find_by_name(fname, lname)
+    data = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        fname = ? AND lname = ?
+    SQL
+
+    data.map { |datum| User.new(datum) }
+  end
+
+  def authored_questions
+    Question.find_by_user_id(@id)
+  end
+
+  def authored_replies
+    Reply.find_by_user_id(@id)
+  end
 end
 
 class Question
