b4264433a0b12600e65814a7958fb1342d26e390
Samuel Chia <cheeeya@berkeley.edu>
2018-02-27 13:38:49 -0800

    Create QuestionFollows initialization

--- aa_questions/lib/questions_database.rb
+++ aa_questions/lib/questions_database.rb
@@ -62,9 +62,20 @@ class QuestionFollows
   attr_accessor :user_id, :question_id
 
   def self.find_by_id(id)
+    data = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        question_follows
+      WHERE
+        id = ?
+    SQL
+
+    data.map { |datum| QuestionFollows.new(datum) }
   end
 
   def initialize(options)
+    @id = options['id']
     @user_id = options['user_id']
     @question_id = options['question_id']
   end
